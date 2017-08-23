ffb44788a7b80ff0319fe352740cf48ddf93187b
767122454 <767122454@qq.com>
2017-08-23 17:23:15 +0800

    update 3-1

---
+++ 3-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("please enter a value:");
+scanf("%d",&a);
+printf("\n");
+printf("please enter b value:");
+scanf("%d",&b);
+c=a+b;
+printf("\n%d",c);
+return 0;
+}
